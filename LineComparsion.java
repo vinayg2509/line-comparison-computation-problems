293497a49671cc82a1c40fe403048c70f18611cc
vinayg2509 <vinayg2509@gmail.com>
2025-04-12 19:07:15 +0530

    [Vinay] Added : Calculated length of a line based on two points

--- LineComparsion.java
+++ LineComparsion.java
@@ -1,5 +1,7 @@
 package linecomparison;
 
+import java.util.Scanner;
+
 class LineComparsion 
 {
 
@@ -7,6 +9,18 @@ class LineComparsion
 	{
 
 		System.out.println("Welcome to line comparsion!!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x1 coordinate : ");
+		int x1 = sc.nextInt();
+		System.out.println("Enter x2 coordinate: ");
+		int x2 = sc.nextInt();
+		System.out.println("Enter y1 coordinate : ");
+		int y1 = sc.nextInt();
+		System.out.println("Enter y2 coordinate : ");
+		int y2 = sc.nextInt();
+		double lengthOfaLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println("The length of a line : " + lengthOfaLine);
+		sc.close();
 	}
 
 }
